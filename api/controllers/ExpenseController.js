8f3247c4bf2fd30f5448c71cca52a77f7e6fe08c
Brayan Vinicius Jordan <brayan.11fera@gmail.com>
2022-08-02 20:04:27 -0300

    Rota para buscar os gastos recentes separando por mes

--- api/controllers/ExpenseController.js
+++ api/controllers/ExpenseController.js
@@ -34,4 +34,39 @@ module.exports = {
 
         return res.json(expense);
     },
+
+    async findRecentExpenses(req, res) {
+        const { user_id } = req.params;
+
+        const user = await models.User.findByPk(user_id);
+
+        if (!user) {
+            return res.status(400).json({ error: 'User not found' });
+        }
+
+        const recentExpenses = await models.Expense.findAll({
+            where: {
+                user_id: user_id
+            }
+        });
+
+        var separateExpenses = [];
+        var dateToFilter = new Date();
+        dateToFilter.setFullYear(dateToFilter.getFullYear() - 1)
+        console.log(dateToFilter);
+
+        for (let i = 0; i < 23; ++i) {
+            dateToFilter.setMonth(dateToFilter.getMonth() + 1);
+            separateExpenses.push({
+                baseMonth: dateToFilter.getMonth() + 1,
+                baseYear: dateToFilter.getFullYear(),
+                expenses: recentExpenses.filter(index =>
+                    index.date.getMonth() == dateToFilter.getMonth() &&
+                    index.date.getFullYear() == dateToFilter.getFullYear()
+                )
+            })
+        }
+
+        return res.json(separateExpenses)
+    }
 }
--- api/routes.js
+++ api/routes.js
@@ -11,5 +11,6 @@ routes.post('/typeexpense/:user_id', TypeExpenseController.create);
 routes.get('/typeexpense/:user_id', TypeExpenseController.findAllByUser);
 
 routes.post('/expense/:user_id', ExpenseController.create);
+routes.get('/expense/:user_id', ExpenseController.findRecentExpenses);
 
 module.exports = routes;
